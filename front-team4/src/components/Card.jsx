5fc60a7bb3abdd22b1f766b02395f425df102613
Lahali <matamua82@gmail.com>
2022-10-17 15:52:47 +0200

    diseño

--- front-team4/src/components/Card.jsx
+++ front-team4/src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div className="w-1/2 bg-white float-right m-20 p-9 drop-shadow-md rounded leading-relaxed text-gray-900">
+    <div className="w-1/2 bg-white float-right m-20  p-9 drop-shadow-md rounded leading-relaxed text-gray-900">
       <h1 className="text-lg">Nombre Barrio</h1>
       <div className="">
         <p>Datos: </p>
@@ -23,7 +23,7 @@ const Card = () => {
         </div>
         </div>
         <div className="flex justify-around p-5">
-        <div className="radial-progress text-green-500 mt-5" style={{ "--value": 80 }}>
+        <div className="radial-progress text-lime-600 mt-5" style={{ "--value": 80 }}>
           0%
         </div>
         <div className="radial-progress text-yellow-500 mt-5" style={{ "--value": 80 }}>
