5b7555bbb98d11a5149c81b3616768b599b57260
Halimeh Habib Diñeiro <halimehhabibdineiro@MacBook-Pro-de-Halimeh.local>
2022-10-17 12:42:15 +0200

    comenzado diseño card

---
+++ front-team4/src/components/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='w-1/2 bg-white float-right m-9 p-5 rounded leading-relaxed'>
+        <h1 className='text-lg'>Nombre Barrio</h1>
+        <div className=''>
+            <p>Datos: </p>
+            <ul>
+                <li>Titulados sueriores y CFGS</li>
+                <li>Número de parados</li>
+                <li>Renta familiar</li>
+                <li>Población mayor de 65 años que vive sola</li>
+                <li>Sobreenvejecimiento</li>
+            </ul>
+        </div>
+
+    </div>
+  )
+}
+
+export default Card
--- front-team4/src/components/Main.jsx
+++ front-team4/src/components/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Card from './Card'
 import Header from './Header'
 
 const Main = () => {
   return (
-    <div>
+    <div className=''>
         <Header/>
+        <Card/>
     </div>
   )
 }
